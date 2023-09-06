e1933bc3bd8a20b57cefd297002928d026b0b8b2
chris1797 <jaehun6004@gmail.com>
2023-09-07 00:34:23 +0900

    create board module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
 
 // 프로젝트의 Root Module
 @Module({
-  imports: [],
+  imports: [BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/board/board.module.ts
@@ -0,0 +1,8 @@
+/**
+ * $ nest g module board 로 모듈 생성 가능
+ */
+
+import { Module } from '@nestjs/common';
+
+@Module({})
+export class BoardModule { }
