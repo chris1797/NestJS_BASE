2314aa615f355b14af9bbda447ef8fc2815a50b2
chris1797 <jaehun6004@gmail.com>
2023-09-06 23:56:37 +0900

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 프로젝트의 Root Module
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
