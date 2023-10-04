473ef0f0355f8f576f320869f6600e8069cd0c17
chris1797 <jaehun6004@gmail.com>
2023-10-04 23:57:25 +0900

    Add board module

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Injectable } from '@nestjs/common';
+import { Body, Controller, Injectable, Post } from '@nestjs/common';
 import { BoardService } from './board.service';
+import { Board } from './board.entity';
 
 @Controller('board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
+
+  @Post('save')
+  async save(@Body() board: Board) {}
 }
