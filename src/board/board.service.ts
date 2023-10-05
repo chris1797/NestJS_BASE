6403a10852cec5537f1ac030ac488a21f154ffa1
chris1797 <jaehun6004@gmail.com>
2023-10-05 23:34:53 +0900

    Add boardService insert&update, detail

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { isEmpty } from '@nestjs/common/utils/shared.utils';
+import * as dayjs from 'dayjs';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardService {
@@ -8,4 +11,24 @@ export class BoardService {
     @InjectRepository(BoardRepository)
     private readonly boardRepository: BoardRepository,
   ) {}
+
+  async insert(board: Board) {
+    if (isEmpty(board)) throw new Error('Can not find User data');
+
+    if (board.id) {
+      const { id, ...newBoard } = board;
+      const result = await this.boardRepository.update(
+        { id: board.id },
+        { ...newBoard },
+      );
+      return !!result.affected;
+    }
+
+    return await this.boardRepository.insert(board);
+  }
+
+  async detail(id: number): Promise<Board> {
+    if (!id || id < 1) throw new Error('Can not find Id');
+    return await this.boardRepository.findOneBy({ id: id });
+  }
 }
