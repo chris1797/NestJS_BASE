d2838d3069a8570a09cc3260415ba10b1608b5a3
chris1797 <jaehun6004@gmail.com>
2023-10-08 23:38:12 +0900

    Add board delete

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,11 +1,27 @@
-import { Body, Controller, Injectable, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Injectable,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { BoardService } from './board.service';
 import { Board } from './board.entity';
+import { DeleteResult } from 'typeorm';
 
 @Controller('board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
   @Post('save')
-  async save(@Body() board: Board) {}
+  async save(@Body() board: Board) {
+    return await this.boardService.insert(board);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return await this.boardService.delete(id);
+  }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -4,6 +4,7 @@ import { BoardRepository } from './board.repository';
 import { isEmpty } from '@nestjs/common/utils/shared.utils';
 import * as dayjs from 'dayjs';
 import { Board } from './board.entity';
+import { DeleteResult } from 'typeorm';
 
 @Injectable()
 export class BoardService {
@@ -30,5 +31,9 @@ export class BoardService {
   async detail(id: number): Promise<Board> {
     if (!id || id < 1) throw new Error('Can not find Id');
     return await this.boardRepository.findOneBy({ id: id });
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.boardRepository.delete(id);
   }
 }
