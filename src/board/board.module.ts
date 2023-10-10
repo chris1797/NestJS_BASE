fa119bd6e2145e6ed183e3286fb4e4bf0c07d9ce
chris1797 <jaehun6004@gmail.com>
2023-10-10 23:55:53 +0900

    board save test

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { Board } from './board.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardController } from './board.controller';
+import { BoardService } from './board.service';
+import { BoardRepository } from './board.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Board])],
+  controllers: [BoardController],
+  providers: [BoardService, BoardRepository],
 })
 export class BoardModule {}
